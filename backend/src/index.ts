51c8f106c59599c67d7b2be586d03e574cc368f1
jahid-md-zysk <rnavaneeth19@gmail.com>
2024-12-06 23:53:36 +0530

    streaming messages added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,20 @@
 require("dotenv").config();
-console.log(process.env.ANTHROPIC_API_KEY)
-console.log("Hello");
+
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic({
+  // defaults to process.env["ANTHROPIC_API_KEY"]
+  apiKey:process.env.ANTHROPIC_API_KEY,
+});
+
+async function main(){
+    anthropic.messages.stream({
+        messages: [{role: 'user', content: "Hello"}],
+        model: 'claude-3-5-sonnet-20241022',
+        max_tokens: 1024,
+}).on('text', (text) => {
+    console.log(text);
+});
+}
+
+main();
